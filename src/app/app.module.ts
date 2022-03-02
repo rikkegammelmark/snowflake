420267cc322176f735271c2d07a14e4a810b36ef
Rikke Bendlin Gammelmark <rikke@toitware.com>
2022-03-02 14:24:03 +0100

    Initial work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MonacoEditorModule } from 'ngx-monaco-editor';
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import { ViewerComponent } from './viewer/viewer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent,
+    ViewerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MonacoEditorModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
